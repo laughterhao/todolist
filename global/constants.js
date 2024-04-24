4a8598ea2344d61f8d2f0067e10f2b05808093d4
laughterhao <laughterhao@gmail.com>
2024-04-24 15:29:37 +0800

    連接Server

--- global/constants.js
+++ global/constants.js
@@ -1,2 +1,2 @@
-export const API_HOST = process.env.NODE_ENV === 'production' ? 'https://todolist-ebon-beta.vercel.app' : 'http://localhost:5000';
+export const API_HOST = process.env.NODE_ENV === 'production' ? 'https://my-json-server.typicode.com/laughterhao/server' : 'http://localhost:5000';
 export const API_GET_DATA = `${API_HOST}/post/1`
